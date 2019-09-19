2d58751e53a09c1f539b1d4fd6c6677841f13578
tarda <tarda@DESKTOP-JQIE7VA>
2019-09-19 20:33:39 +0200

    *

--- js/cifo.js
+++ js/cifo.js
@@ -2,58 +2,62 @@
  * JS CIFO Web
  */
 
-window.addEventListener("load",includeHTML);
-
+window.addEventListener("load", includeHTML);
 
 function includeHTML() {
-  //console.log(page);
-  var z, i,  elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
- 
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("data-include");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-        	  elmnt.outerHTML = this.responseText;
-        	  activeMenu();
-          }
-          if (this.status == 404) {elmnt.outerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          //elmnt.removeAttribute("data-include");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
+	// console.log(page);
+	var z, i, elmnt, file, xhttp;
+	/* loop through a collection of all HTML elements: */
+	z = document.querySelectorAll("[data-include]");
+
+	for (i = 0; i < z.length; i++) {
+		elmnt = z[i];
+		/* search for elements with a certain atrribute: */
+		file = elmnt.getAttribute("data-include");
+		if (file) {
+			/* make an HTTP request using the attribute value as the file name: */
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					if (this.status == 200) {
+						// debugger;
+						elmnt.outerHTML = this.responseText;
+						activeMenu();
+					}
+					if (this.status == 404) {
+						elmnt.outerHTML = "Page not found.";
+					}
+					/* remove the attribute, and call this function once more: */
+					// elmnt.removeAttribute("data-include");
+					includeHTML();
+				}
+			}
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			/* exit the function: */
+			return;
+		}
+	}
+	0
 };
 
-function activeMenu(){
-	var active, uri;
-	uri =  window.location.pathname.split( '/' )[1];
+function activeMenu() {
+	var i, breadc, active, uri;
+	uri = window.location.pathname.split('/')[1];
+
+	active = document.querySelectorAll('[data-active]');
+	for (i = 0; i < active.length; i++) {
+		if (active[i].dataset.active == uri) {
+			active[i].className += " active "
+		}
+	}
 	debugger;
-	active = document.querySelectorAll("[target=uri]")
+	breadc = document.querySelectorAll('[aria-label="breadcrumb"]');
+	if (breadc[0]) {
+		if (uri == "Home.html") {
+			breadc[0].innerHTML = '<ol class="breadcrumb"> <li class="breadcrumb-item active" aria-current="page">Home</li></ol>';
+		} else {
+			breadc[0].innerHTML = '<ol class="breadcrumb"><li class="breadcrumb-item" aria-current="page">Home</li><li class="breadcrumb-item active" aria-current="page">uri</li></ol>'
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
