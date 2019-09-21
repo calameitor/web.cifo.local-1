2d3b01c916ac2c90c4b0ed7bb7a2c96a19c7e337
MarcEmp <MarcEmp@192.168.1.129>
2019-09-21 23:07:19 +0200

    *

--- js/cifo.js
+++ js/cifo.js
@@ -65,12 +65,6 @@ function activeMenu() {
 	}
 }
 
-function myMap() {
-	var mapProp = {
-		center : new google.maps.LatLng(51.508742, -0.120850),
-		zoom : 5,
-	};
-	var map = new google.maps.Map(document.getElementById("cifoMap"), mapProp);
-}
+
 
 
