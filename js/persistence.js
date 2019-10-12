9c618c9795abb27b8e121460977020a0ccca649d
MarcEmp <MarcEmp@192.168.1.129>
2019-10-12 15:19:02 +0200

    Cookie Check

---
+++ js/persistence.js
@@ -0,0 +1,20 @@
+//CHECkCHECKCHECK
+if (!detectCookie('rgpda')){
+	debugger;
+	var cRG=document.getElementById("boxRGPD");
+	cRG.classList.add("d-none")
+}
+
+window.addEventListener("load",InitEvent)
+
+function InitEvent(e){
+	var RGPDBtt=document.getElementById("btnRGPD");
+	RGPDBtt.addEventListener ("click",(e)=>{
+		var cRG=document.getElementById("boxRGPD");
+		setCookie('rgpda',1,1000);
+		cRG.classList.add("d-none")
+	})
+	
+}
+
+	
